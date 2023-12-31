8fb8f15487681ed568af53f3e609f4649cdb0cf0
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-12-31 14:37:56 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -11,12 +11,15 @@ def is_enabled(value, default):
         return default
 
 SESSION = environ.get("SESSION", "forward bot")
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-BOT_TOKEN = environ["BOT_TOKEN"]
+API_ID = int(os.environ.get("API_ID", "13859375"))
+API_HASH = os.environ.get("API_HASH", "6ae4ba5fdc4eb0948616585a5bb7ee58")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6424576681:AAECKahVJ8E6BZ6lwi4gz-W9RBcpBJGhQec")
+
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001924870738"))
 PORT = int(environ.get("PORT", "8080"))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2034319320').split()]
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/Sridhark01/Forward-bot-beta-new")
 DB_URI = environ.get('DB_URI', "")
-DB_NAME = environ.get('DB_NAME', "")
+DB_NAME = environ.get('DB_NAME', "cluster0")
+
+
